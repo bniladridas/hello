093cdaed0840a1e083f6bdcd8d04186fb0d5b684
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-10-08 04:48:19 +0530

    fix: switch to hashrouter for cypress compatibility

--- core/components/App/App.test.js
+++ core/components/App/App.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 test('renders Hello title', () => {
   render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   )
   const titleElement = screen.getByText(/Hello/i)
   expect(titleElement).toBeInTheDocument()
